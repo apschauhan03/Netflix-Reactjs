758548ae13e6b2ab50ef601f2aeea7afe0f51c5d
Abhijit Singh <abhijitchau8948@gmail.com>
2023-06-23 01:00:55 +0530

    Navbar initial commit

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+
+function Homepage() {
+  return (
+    <>
+        {/* Navbar */}
+        <Navbar />
+        <div>
+        <h1 style={{margin:0,padding:0}}>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          </div>
+        {/* Banner */}
+        {/* Rows */}
+    </>
+  )
+}
+
+export default Homepage
---
+++ src/components/Homepage/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import "./Navbar.css"
+import logo from './transparent_logo.png';
+import avatar from './avatar.jpg';
+
+function Navbar() {
+  const [show,handleShow] = useState(false);
+  const transitionNavbar = ()=>{
+    if(window.scrollY>100)
+    {
+      handleShow(true);
+    }
+    else
+      handleShow(false);
+  } 
+  useEffect(()=>{
+    window.addEventListener("scroll",transitionNavbar);
+    return ()=>{
+      window.removeEventListener("scroll",transitionNavbar);
+    }
+  },[])
+
+
+  return (
+    <div className='nav'>
+        <div className={`nav_container ${show&&"nav_black"}`}>
+          <img className='nav_logo nav_container_contents' src={logo}></img>
+          <img className='nav_avatar nav_container_contents' src={avatar} alt='nav_avatar'></img>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
